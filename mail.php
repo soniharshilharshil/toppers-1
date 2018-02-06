449cc255815c993e7e01c340f0530a8179100e88
vaibhavshah9537 <33410633+vaibhavshah9537@users.noreply.github.com>
2018-02-06 10:14:31 +0530

    services

--- mail.php
+++ mail.php
@@ -1,27 +1,18 @@
 <?php
-error_reporting(E_ALL);
-require("../toppers/PHPMailer_5.2.4/class.phpmailer.php");
-$mail = new PHPMailer();
-$mail->IsSMTP(); // set mailer to use SMTP
-$mail->SMTPDebug  = 2;
-$mail->From = "vaibhav.sanskar@gmail.com";
-$mail->FromName = "Vaibhav Shah";
-$mail->Host = "smtp.gmail.com"; // specif smtp server
-$mail->SMTPSecure= "ssl"; // Used instead of TLS when only POP mail is selected
-$mail->Port = 465; // Used instead of 587 when only POP mail is selected
-$mail->SMTPAuth = true;
-$mail->Username = "vaibhav.sanskar@gmail.com"; // SMTP username
-$mail->Password = "Vaibhav@1125"; // SMTP password
-$mail->AddAddress("Vaibhav.sanskar@gmail.com", "Jiansen"); //replace myname and mypassword to yours
-$mail->AddReplyTo("jiansentest@gmail.com", "Jiansen");
-$mail->WordWrap = 50; // set word wrap
-//$mail->AddAttachment("c:\\temp\\js-bak.sql"); // add attachments
-//$mail->AddAttachment("c:/temp/11-10-00.zip");
+$to = "vaibhav.sanskar@gmail.com";
+$subject = "HTML email";
 
-$mail->IsHTML(true); // set email format to HTML
-$mail->Subject = 'test';
-$mail->Body = 'test';
+$message = "
+TEST MAIL
+";
 
-if($mail->Send()) {echo "Send mail successfully";}
-else {echo "Send mail fail";}
-?>
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: vaibhav_a_shah@outlook.com' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+?> 
